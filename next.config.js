314d309ccdcaa299d66b447094a5d4389a7d5cc8
SridharIyappan <sridharsp4774@gmail.com>
2022-08-24 15:27:16 +0530

    Navbar Donte link and css for this back end ip changed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   env: {
-    DOMAIN_NAME: "http://3.109.200.71",
+    DOMAIN_NAME: "http://65.2.6.154",
     // DOMAIN_NAME: "http://localhost:3002",
     // i18n,
   },
